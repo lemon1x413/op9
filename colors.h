1631a07244e1162c2ae17ff4613c610907c72059
lemon1x413 <romalemontop@gmail.com>
2025-01-03 15:29:34 +0200

    999(FINAL)

--- colors.h
+++ colors.h
@@ -3,9 +3,6 @@
 
 #define RED "\e[0;31m"
 #define GREEN "\e[0;32m"
-#define ITALIC_GREEN "\e[3;32m"
-#define BLUE "\e[0;36m"
-#define DARK_BLUE "\e[0;34m"
 #define YELLOW  "\e[0;33m"
 #define RESET "\e[0;0m"
 
